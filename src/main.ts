a4f1ff147c7c6629f487d6537e25b282fb2e9371
horrible little slime <69secret69email69@gmail.com>
2021-06-07 17:52:23 -0400

    updating main to reflect previous changes

--- src/main.ts
+++ src/main.ts
@@ -55,7 +55,10 @@ export function main(args: string) {
     } else if (args.includes("buff")) {
         const turnZero = args.includes("0") || args.includes("zero");
         buffUp(turnZero);
+    } else if (args.includes("free fights")) {
+        freeFights();
     } else if (args.includes("day") || args.includes("full")) {
+        buffUp(true);
         freeFights();
         prepBlocks();
         runBlocks();
