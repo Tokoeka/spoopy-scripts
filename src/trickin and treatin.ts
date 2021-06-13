f175b432f35995dce02f7fa121e14d1256728b00
horrible little slime <69secret69email69@gmail.com>
2021-06-13 18:44:47 -0400

    don't blindly replace ds with ls in a regexp, my guy

--- src/trickin and treatin.ts
+++ src/trickin and treatin.ts
@@ -85,9 +85,10 @@ function treat() {
         } else if (thisBlock.match(RegExp(`whichhouse=${i}>[^>]*?starhouse`))) {
             visitUrl(`choice.php?whichchoice=804&option=3&whichhouse=${i}&pwd`);
             runChoice(2);
+            /value="\l+/;
         }
     }
-    if (block().match(RegExp(`whichhouse=\l+>[^>]*?house_l`)))
+    if (block().match(RegExp(`whichhouse=\d+>[^>]*?house_l`)))
         throw "I thought I was out of light houses, but I wasn't. Alas!";
 }
 
